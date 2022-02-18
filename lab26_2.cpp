7c55439ba97d25c2439ba9dd2291d3683e0111fd
tanawat640610678 <thanawat_chai@cmu.ac.th>
2022-02-18 17:04:08 +0700

    Lab26_2.cpp Complete

--- lab26_2.cpp
+++ lab26_2.cpp
@@ -5,27 +5,46 @@
 using namespace std;
 
 void printList(list<string> s){
-	list<string>::iterator i;
-	for( i = s.begin(); i != s.end(); i++){
-		cout << *i << " ";
-	}		
-	cout << "\n";	
+ list<string>::iterator i;
+ for( i = s.begin(); i != s.end(); i++){
+  cout << *i << " ";
+ }  
+ cout << "\n"; 
 }
 
 int main(){
-	
-	list<string> line_up;
-	list<string>::iterator loc;
-	
-	line_up.push_back("Alice");
-	line_up.push_back("Bob");
-	
-	loc = find(line_up.begin(),line_up.end(),"Bob");
-	line_up.insert(loc,"Oscar");
-	
-	//Write your code here
-	
-	printList(line_up);
-		
-	return 0;
+ 
+ list<string> line_up;
+ list<string>::iterator loc;
+ 
+ line_up.push_back("Alice");
+ line_up.push_back("Bob");
+ 
+ loc = find(line_up.begin(),line_up.end(),"Bob");
+ line_up.insert(loc,"Oscar");
+
+ line_up.push_back("Luffy");
+ line_up.push_back("Sanji");
+ line_up.push_back("Nami");
+
+ for(int i = 0; i < 2; i++) 
+     line_up.pop_front();
+ 
+ loc = find(line_up.begin(),line_up.end(),"Luffy");
+ line_up.insert(++loc,"Narutu");
+
+ line_up.insert(line_up.begin(),"Prayath");
+
+ loc = find(line_up.begin(),line_up.end(),"Prayath");
+ line_up.insert(++loc,"Tony");
+
+ loc = find(line_up.begin(),line_up.end(),"Bob");
+ line_up.erase(loc);
+ 
+ for(int i = 0; i < 3; i++) 
+     line_up.pop_front();
+
+ printList(line_up);
+  
+ return 0;
 }
